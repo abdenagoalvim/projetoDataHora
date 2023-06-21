2c85d27c69aa336dc3bbc60fa09fa5fbadb933e6
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-21 20:44:16 -0300

    incluído atributos e métodos construtores

--- src/projetoDataHora/Data.java
+++ src/projetoDataHora/Data.java
@@ -1,5 +1,18 @@
 package projetoDataHora;
 
 public class Data {
+	private int dia;
+	private int mes;
+	private int ano;
+	String mascara = "DD/MM/AAAA";
+	
+	public Data() {
+	}
 
+	public Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
 }
