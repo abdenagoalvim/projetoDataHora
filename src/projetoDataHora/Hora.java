cca5fe6e85954b94fa4905e2e1a514349afe780c
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-21 20:47:37 -0300

    incluído atributos e métodos construtores

--- src/projetoDataHora/Hora.java
+++ src/projetoDataHora/Hora.java
@@ -1,5 +1,18 @@
 package projetoDataHora;
 
 public class Hora {
+	private int hor;
+	private int min;
+	private int seg;
+	String mascara = "HH/MM";
+	
+	public Hora() {
+	}
 
+	public Hora(int hor, int min, int seg) {
+		this.hor = hor;
+		this.min = min;
+		this.seg = seg;
+	}
+	
 }
