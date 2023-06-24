58513219a4a8ac73f4c13de0aa783857ed4fc8e1
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-24 11:05:27 -0300

    teste de objeto do tipo Data

--- src/projetoDataHora/Principal.java
+++ src/projetoDataHora/Principal.java
@@ -3,7 +3,27 @@ package projetoDataHora;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		Data d1 = new Data(7,2,2023);
+		System.out.println(d1);
+		d1.setMascara("D/M/AAAA");
+		System.out.println(d1);
+		d1.setMascara("D/M/AA");
+		System.out.println(d1);
+		d1.setMascara("DD/MMM/AAAA");
+		System.out.println(d1);
+		d1.setMascara("Belo Horizonte, D de MMMM de AAAA.");
+		System.out.println(d1);
+		d1.setDia(13);
+		d1.setMes(9);
+		d1.setMascara("D/M/AA");
+		System.out.println(d1);
+		d1.setMascara("D/M/AAAA");
+		System.out.println(d1);
+		d1.setMascara("DD/MM/AAAA");
+		System.out.println(d1);
+		d1.setMascara("DD/MMM/AAAA");
+		System.out.println(d1);
+		d1.setMascara("Belo Horizonte, D de MMMM de AAAA.");
+		System.out.println(d1);
 	}
-
 }
