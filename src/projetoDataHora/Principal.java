c294c2c62ba5756b3c1f8dde724c4f12dc91cff1
Abdenago Alvim <abdenago.alvim@gmail.com>
2023-06-24 11:25:48 -0300

    teste de objeto do tipo Hora

--- src/projetoDataHora/Principal.java
+++ src/projetoDataHora/Principal.java
@@ -25,5 +25,19 @@ public class Principal {
 		System.out.println(d1);
 		d1.setMascara("Belo Horizonte, D de MMMM de AAAA.");
 		System.out.println(d1);
+		
+		System.out.println();
+		
+		Hora h1 = new Hora(9, 5);
+		h1.setSeg(25);
+		System.out.println(h1);
+		h1.setMascara("H:M:S");
+		System.out.println(h1);
+		h1.setMascara("HH:MM:SS");
+		System.out.println(h1);
+		h1.setMascara("MM:SS");
+		System.out.println(h1);
+		
+		
 	}
 }
